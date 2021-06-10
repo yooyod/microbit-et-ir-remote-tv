bebe5080c8eb53b9d191e19f45d1b190873f232f
Sittiphol Yooyod <20336055+yooyod@users.noreply.github.com>
2021-06-10 15:52:47 +0700

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+
+ 
+ IR_RemoteTV.onIrDatagram(function () 
+ {
+    basic.showNumber(IR_RemoteTV.irButton())
+})
+IR_RemoteTV.connectIrReceiver(DigitalPin.P11)
+
+
+
+//© 2021 GitHub, Inc.
